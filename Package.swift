c4e1ef37c2359e009b34fbba0ba13a115a912a35
xncbf <xncbf12@gmail.com>
2024-12-14 19:29:05 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["NMapsMapTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/navermaps/SPM-NMapsGeometry.git", from: "1.0.2")
+        .package(url: "https://github.com/xncbf/SPM-NMapsGeometry.git", from: "1.0.2")
     ],
     targets: [
         .target(
